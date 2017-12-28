7541d344b9cc3d0b5748d945a2329662d81f6c6b
Paras Varshney <paras.varshney97@gmail.com>
2017-12-28 22:47:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paras/parasvarshney/ParasVarshney.java
@@ -0,0 +1,13 @@
+package com.example.paras.parasvarshney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParasVarshney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paras_varshney);
+    }
+}
